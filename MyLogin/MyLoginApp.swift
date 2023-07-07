22a020553824014959aacaaf84ecf92b698ffc92
FIRAT ÖREN <oren_2011@hotmail.com>
2023-07-07 15:05:13 +0300

    Initial Commit

---
+++ MyLogin/MyLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoginApp.swift
+//  MyLogin
+//
+//  Created by Fırat Ören on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
